078673eb8d3118641bd3f7b4175556657639118b
John-Pi <john.picolton@gmail.com>
2017-05-31 17:08:13 +0200

    jeu

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -2,48 +2,78 @@ package model;
 
 import tools.ChessPiecesFactory;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
  * Created by Johnpi on 31/05/2017.
  */
-public class Jeu implements Game{
-    private  Couleur couleur;
+public class Jeu implements Game {
+    private Couleur couleur;
     private List<Pieces> listPiece;
 
     public Jeu(Couleur couleur) {
         this.couleur = couleur;
-        listPiece = new ArrayList<>();
+
         listPiece = ChessPiecesFactory.newPieces(couleur);
     }
+
+    private Pieces findPiece(int x, int y) {
+        Pieces piece1 = null;
+        for (Pieces piece : listPiece) {
+            if (piece.getX() == x && piece.getY() == y) {
+                piece1 = piece;
+            }
+        }
+        return piece1;
+    }
+
 
     @Override
     public String toString() {
         String desc = "";
-        for (Pieces piece:listPiece){
-            desc = desc+piece.toString();
+        for (Pieces piece : listPiece) {
+            desc = desc + " " + piece.toString();
         }
         return desc;
     }
 
     @Override
     public boolean isPieceHere(int x, int y) {
-        return false;
+        return (findPiece(x, y) != null);
     }
 
     @Override
     public boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
-        return false;
+        Pieces piece = findPiece(xInit, yInit);
+        if (piece != null) {
+            if (piece.isMoveOk(xFinal, yFinal, true, true)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
-        return false;
+        ;
+        Pieces piece = findPiece(xInit, yInit);
+        if (piece != null) {
+            piece.move(xFinal, yFinal);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public boolean capture(int xCatch, int yCatch) {
         return false;
     }
+
 }
