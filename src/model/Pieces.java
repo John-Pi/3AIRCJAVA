8cff9fe10f818cb7bd53a7ca2b9ddba7412ead12
pioupiouk <collin.d@live.fr>
2017-06-02 15:19:50 +0200

    doc Pieces propre

--- src/model/Pieces.java
+++ src/model/Pieces.java
@@ -9,41 +9,62 @@ package model;
  */
 public interface Pieces {
 
-    /*
-    * @return OK si la piece a le droit de capturer la piece qui se trouve 
+    /**
+     *
+     * @return OK si la piece a le droit de capturer la piece qui se trouve 
     * aux coordonnées finales. 
-    */
+     */
+
     public boolean capture();
 
-    /*
-    * @return la couleur de la piece
-    */
+    
+    /**
+     *
+     * @return la couleur de la piece
+     */
+
     public Couleur getCouleur();
 
-    /*
-    * @return le nom de la piece
-    */
+    /**
+     *
+     * @return le nom de la piece
+     */
+
     public String getName();
 
-    /*
-    * @return l'indice de la colonne ou est positionnee la piece
-    */
+    /**
+     *
+     * @return l'indice de la colonne ou est positionnee la piece
+     */
+
     public int getX();
 
-    /*
-    * @return l'indice de la  ligne ou est positionnee la piece
-    */
+    /**
+     *
+     * @return l'indice de la  ligne ou est positionnee la piece
+     */
+
     public int getY();
 
-    /*
-    * @return true si déplacement légal en fonction du mouvement specifique 
-    * de chaque pièce
-    */
+    /**
+     *
+     * @param xFinal
+     * @param yFinal
+     * @param isCatchOk
+     * @param isCastlingPossible
+     * @return true si déplacement légal en fonction du mouvement specifique 
+     * de chaque pièce
+     */
+
     public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible);
 
-    /*
-    * @return true si deplacement effectif
-    */
+    /**
+     *
+     * @param xFinal
+     * @param yFinal
+     * @return true si deplacement effectif
+     */
+
     public boolean move(int xFinal, int yFinal);
 
 }
